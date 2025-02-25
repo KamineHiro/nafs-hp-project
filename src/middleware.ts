741be8b98056755d07452a6cf7a430de2ff77f58
com.hirokiakamine <hiroki080602@icloud.com>
2025-02-26 00:46:26 +0900

    middleware.ts update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,31 @@
+import { NextRequest, NextResponse } from 'next/server'
 import { withAuth } from "next-auth/middleware"
+
+// Basic認証の関数
+function basicAuth(req: NextRequest) {
+  const auth = req.headers.get('authorization')
+  const password = process.env.VERCEL_PROTECT_PASSWORD
+
+  if (!auth || auth !== `Basic ${Buffer.from(`admin:${password}`).toString('base64')}`) {
+    return new NextResponse('Unauthorized', {
+      status: 401,
+      headers: {
+        'WWW-Authenticate': 'Basic realm="Secure Area"',
+      },
+    })
+  }
+
+  return null
+}
 
 // /admin/login 以外の /admin/* パスを保護
 export default withAuth(
-  function middleware() {
+  function middleware(req) {
+    // 本番環境でのみBasic認証を有効にする
+    if (process.env.NODE_ENV === 'production') {
+      const basicAuthResult = basicAuth(req)
+      if (basicAuthResult) return basicAuthResult
+    }
     return
   },
   {
